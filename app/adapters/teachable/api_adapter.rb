d83b475cd7fe93805692dea1151d366363d31a0c
Igor Eineck <hauzip@gmail.com>
2025-09-07 21:05:36 -0300

    Extract API request behavior to an adapter

---
+++ app/adapters/teachable/api_adapter.rb
@@ -0,0 +1,67 @@
+require "uri"
+require "net/http"
+
+module Teachable
+  class ApiAdapter
+    BASE_API_URL = "https://developers.teachable.com/v1"
+
+    def self.fetch_published_courses
+      url = URI("#{BASE_API_URL}/courses?is_published=true")
+      response = build_request(url:)
+
+      if response.code == "200"
+        body = JSON.parse(response.read_body)
+
+        { courses: body["courses"], meta: body["meta"] }
+      else
+        { courses: [], meta: {} }
+      end
+    end
+
+    def self.fetch_active_course_enrollments(course_id:)
+      url = URI("#{BASE_API_URL}/courses/#{course_id}/enrollments")
+      response = build_request(url:)
+
+      if response.code == "200"
+        body = JSON.parse(response.read_body)
+        enrollments = body["enrollments"].select { |enrollment| enrollment_active?(enrollment) }
+
+        { enrollments: enrollments, meta: body["meta"] }
+      else
+        { enrollments: [], meta: {} }
+      end
+    end
+
+    def self.fetch_user(user_id:)
+      url = URI("#{BASE_API_URL}/users/#{user_id}")
+      response = build_request(url:)
+
+      if response.code == "200"
+        body = JSON.parse(response.read_body)
+
+        { user: body }
+      else
+        { user: {} }
+      end
+    end
+
+    private
+
+    def self.build_request(url:)
+      http = Net::HTTP.new(url.host, url.port)
+      http.use_ssl = true
+
+      request = Net::HTTP::Get.new(url)
+      request["accept"] = "application/json"
+      request["apiKey"] = ENV.fetch("TEACHABLE_API_KEY")
+
+      http.request(request)
+    end
+
+    def self.enrollment_active?(enrollment)
+      return Date.today >= Date.parse(enrollment["enrolled_at"]) if enrollment["expires_at"].blank?
+
+      Date.today >= Date.parse(enrollment["enrolled_at"]) && Date.today <= Date.parse(enrollment["expires_at"])
+    end
+  end
+end
--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,74 +1,21 @@
-# require "http"
-require "uri"
-require "net/http"
-
 class CoursesController < ApplicationController
-  # TODO: filter published courses only. It can be passed as a query param:
   def index
-    url = URI("https://developers.teachable.com/v1/courses?published=true")
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true
+    data = Teachable::ApiAdapter.fetch_published_courses
 
-    request = Net::HTTP::Get.new(url)
-    request["accept"] = "application/json"
-    request["apiKey"] = ENV.fetch("TEACHABLE_API_KEY")
-
-    response = http.request(request)
-
-    if response.code == "200"
-      body = JSON.parse(response.read_body)
-      @courses = body["courses"]
-      @meta = body["meta"]
-    else
-      # just log for now
-    end
+    @courses = data[:courses]
+    @meta = data[:meta]
   end
 
   def show
-    url = URI("https://developers.teachable.com/v1/courses/#{params[:id]}/enrollments")
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true
+    data = Teachable::ApiAdapter.fetch_active_course_enrollments(course_id: params[:id])
 
-    request = Net::HTTP::Get.new(url)
-    request["accept"] = "application/json"
-    request["apiKey"] = ENV.fetch("TEACHABLE_API_KEY")
-
-    response = http.request(request)
-
-    if response.code == "200"
-      body = JSON.parse(response.read_body)
-      @enrollments = body["enrollments"].select do |enrollment|
-        if enrollment["expires_at"].present?
-          Date.today >= Date.parse(enrollment["enrolled_at"]) && Date.today <= Date.parse(enrollment["expires_at"])
-        else
-          Date.today >= Date.parse(enrollment["enrolled_at"])
-        end
-      end
-
-      @meta = body["meta"]
-    else
-      # just log for now
-    end
+    @enrollments = data[:enrollments]
+    @meta = data[:meta]
   end
 
   def fetch_user
-    url = URI("https://developers.teachable.com/v1/users/#{params[:user_id]}")
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true
+    data = Teachable::ApiAdapter.fetch_user(user_id: params[:user_id])
 
-    request = Net::HTTP::Get.new(url)
-    request["accept"] = "application/json"
-    request["apiKey"] = ENV.fetch("TEACHABLE_API_KEY")
-
-    response = http.request(request)
-
-    if response.code == "200"
-      body = JSON.parse(response.read_body)
-      @user = body
-
-      render partial: "courses/user", locals: { user: @user }
-    else
-      # just log for now
-    end
+    render partial: "courses/user", locals: { user: data[:user] }
   end
 end
